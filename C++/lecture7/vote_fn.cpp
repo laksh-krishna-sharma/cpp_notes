948fb1fbbeb60e845408cf92c9dcb6e10a599076
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-09-27 21:25:42 +0530

    lecture 7 and 8

---
+++ C++/lecture7/vote_fn.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+bool vote(int age)
+{
+    if(age>=18)
+    {
+        return true;
+    }
+    return false;
+}
+int main()
+{
+    cout<<"Enter the age : ";
+    int age;
+    cin>>age;
+    if(vote(age)==true)
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+        cout<<"No";
+    }
+
+    return 0;
+
+}
