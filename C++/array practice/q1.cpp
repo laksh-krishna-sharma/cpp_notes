c66c201e697ed6df22192619bceb15fd20dd2c2f
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-12-09 19:22:07 +0530

    array

--- C++/array practice/q1.cpp
+++ C++/array practice/q1.cpp
@@ -1,14 +1,11 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    
-    int arr1[] = {0,2,3,4};
-    int arr2[] = {1,2,5,7,8,9,11,23};
+int answer(int arr1[],int arr2[]){
 
     int ans[12];
     int i = 0,j = 0,k = 0;
-    while (i<4 && j<8)
+    while (i<4 || j<8)
     {
         if (arr1[i]>arr2[j])
         {
@@ -23,6 +20,17 @@ int main(){
             i++;
         }
     }
+    for (int l = 0; l < 12; l++)
+    {
+        cout<<ans[l]<<" ";
+    }
     
+}
+
+int main(){
+    
+    int arr1[] = {0,1,3,4};
+    int arr2[] = {5,6,7,8,9,10,11,23};
+
     return 0;
 }
