3bbf9ad877cbccb35931214e41045c9542b7bea1
laksh-krishna-sharma <asklakshsharma@gmail.com>
2023-12-25 18:55:40 +0530

    lecture15

---
+++ C++/lecture15/matrixRotation.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int rotatedMatrix(vector<vector<int>> &v){
+    // transpose
+    for (int i = 0; i < v.size(); i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            swap(v[i][j],v[j][i]);
+        }
+        
+    }
+    // reverse
+    for (int i = 0; i < v.size(); i++)
+    {
+        reverse(v[i].begin(),v[i].end());
+    }
+    
+}
+
+int main(){
+    int n;
+    cout<<"enter number rows and column : ";
+    cin>>n;
+    cout<<endl;
+
+    vector<vector<int>> vec(n,vector<int>(n));
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout<<"("<<i<<","<<j<<") : ";
+            cin>>vec[i][j];
+        }
+        cout<<endl;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout<<vec[i][j]<<" ";
+        }
+        cout<<endl;
+    }cout<<endl;
+
+    int ans = rotatedMatrix(vec);
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout<<vec[i][j]<<" ";
+        }
+        cout<<endl;
+    }cout<<endl;
+
+    return 0;    
+}
