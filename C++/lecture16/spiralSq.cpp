1d5d2cfe6d6f7cb9bec347e3ac67054fcf84d697
laksh-krishna-sharma <asklakshsharma@gmail.com>
2024-02-04 05:44:34 +0530

    2DArray

--- C++/lecture16/spiralSq.cpp
+++ C++/lecture16/spiralSq.cpp
@@ -4,20 +4,23 @@
 #include<algorithm>
 using namespace std;
 
-void spiralOrder(vector<vector<int>>& matrix) {
-    
+vector<vector<int>> spiralSq(int n) {
+
+    vector<vector<int>> matrix(n, vector<int>(n));
+
     int left = 0;
     int right = matrix[0].size() - 1;
     int top = 0;
     int bottom = matrix.size() - 1;
 
     int dir = 0;
+    int val = 1;
 
     while (left <= right && top <= bottom) {
         if (dir == 0) {
             // left to right 
             for (int col = left; col <= right; col++) {
-                cout << matrix[top][col] << " ";
+                matrix[top][col] = val++;
             }
             top++;
         }
@@ -25,7 +28,7 @@ void spiralOrder(vector<vector<int>>& matrix) {
         else if (dir == 1) {
             // top to bottom
             for (int row = top; row <= bottom; row++) {
-                cout << matrix[row][right] << " ";
+                matrix[row][right] = val++;
             }
             right--;
         }
@@ -33,7 +36,7 @@ void spiralOrder(vector<vector<int>>& matrix) {
         else if (dir == 2) {
             // right to left
             for (int col = right; col >= left; col--) {
-                cout << matrix[bottom][col] << " ";
+                matrix[bottom][col] = val++;
             }
             bottom--;
         }
@@ -41,7 +44,7 @@ void spiralOrder(vector<vector<int>>& matrix) {
         else {
             // bottom to top
             for (int row = bottom; row >= top; row--) {
-                cout << matrix[row][left] << " ";
+                matrix[row][left] = val++;
             }
             left++;
         }
@@ -49,27 +52,26 @@ void spiralOrder(vector<vector<int>>& matrix) {
         // dir = 0,1,2,3
         dir = (dir + 1) % 4;
     }
+
+    return matrix;
 }
 
 int main() {
     
-    int n, m;
-    cout << "enter number of rows: ";
+    int n;
+    cout << "enter number of rows and column : ";
     cin >> n;
-    cout << "enter number of columns: ";
-    cin >> m;
 
-    vector<vector<int>> matrix(n, vector<int>(m));
+    vector<vector<int>> matrix(n, vector<int>(n));
+
+    matrix = spiralSq(n);
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cout << "(" << i << "," << j << ") : ";
-            cin >> matrix[i][j];
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
         }
         cout << endl;
-    }
-
-    spiralOrder(matrix);
+    }    
 
     cout<<endl;
 
