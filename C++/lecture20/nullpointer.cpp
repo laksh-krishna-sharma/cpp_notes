22faf4ad0a7d044905495d9d73a41d2bcb9a9d66
laksh-krishna-sharma <asklakshsharma@gmail.com>
2024-02-15 15:03:21 +0000

    pointers3

---
+++ C++/lecture20/nullpointer.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int *ptr = NULL; // ye ek null pointer hai jo 0*0 par pointe kr ta hai
+    //  assigning NULL , 0 , '/0'  all are same
+    cout << ptr << " " << *ptr << endl;
+
+    return 0;
+}
