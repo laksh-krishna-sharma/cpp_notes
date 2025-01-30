ea5a89c877d7bae13f37f38ae0c68b3de712788a
laksh-krishna-sharma <asklakshsharma@gmail.com>
2025-01-30 15:33:08 +0530

    start

--- C++/lecture18/addressOperator.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int x = 10;
-    cout << &x << endl;
-
-    return 0;
-}
--- C++/lecture20/wildpointer.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int *ptr;  // pointer jisa koi bhi address assigne na ho  to wo ek random memory location par point kr ta hai jisa wild pointer bol te hai
-    cout << ptr << endl;  
-
-    return 0;
-}
